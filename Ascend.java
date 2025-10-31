02fb8f7d446156e25bbb4a6eec7b96ceaecbd480
Dana Somekh <dana.somekh@post.runi.ac.il>
2025-10-31 18:22:25 +0200

    completed assignment

--- Ascend.java
+++ Ascend.java
@@ -1,7 +1,14 @@
-// Generates three integer random numbers in a given range,
-// and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
-	}
+		int limit = Integer.parseInt(args[0]); // להבין איך ניתן להגביל ערכים מעל 0
+		int a = (int) (Math.random() * limit);
+		int b = (int) (Math.random() * limit);
+		int c = (int) (Math.random() * limit);
+		int maxAB = Math.max(a,b);
+		int maxABC = Math.max(maxAB,c);
+		int minAB = Math.min(a,b);
+		int minABC = Math.min(minAB,c);
+		int midABC = (a + b + c) - minABC - maxABC;
+		System.out.println(minABC + " " + midABC + " " + maxABC);
+		}
 }
--- Bill3.java
+++ Bill3.java
@@ -1,8 +1,11 @@
-// Splits a restaurant bill evenly among three diners.
 public class Bill3 {
 	public static void main(String[] args) {
-		// To get you started, here is the first line in the program:
 	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+		String name2 = args[1];
+		String name3 = args[2];
+		int amount = Integer.parseInt(args[3]);
+		double dividedamount = amount / 3;
+		double finalamount = Math.ceil(dividedamount);
+		System.out.println("Dear " + name3 + " " + name2 + " and " + name1 + ": pay " + finalamount + " Shekels each.");
 	}
 }
